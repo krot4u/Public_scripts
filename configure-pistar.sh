719aa59c84d58b1b67c74a82ff6869500c088c16
Victor Kro <krot4u@gmail.com>
2023-01-30 16:13:56 +0300

    yes

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sed -i "s/curl --fail -o \$\{DMRHOSTS\}/#curl --fail -o \$\{DMRHOSTS\}/" /usr/local/sbin/HostFilesUpdate.sh
-
-sed -i "s/curl --fail -o /$\{DMRHOSTS}/#curl --fail -o /$\{DMRHOSTS}/" ./HostFilesUpdate.sh
+curl --fail -o /usr/local/sbin/HostFilesUpdate.sh 
