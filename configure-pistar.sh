e40e50080628adcf9d668c05399c4b7a41de92e4
Victor Kro <krot4u@gmail.com>
2023-01-30 18:04:12 +0300

    fix

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# CONFIG_DMRID=5973272; CONFIG_CALLSIGN=TURBO; CONFIG_FREQUENCY=433500000
+# export CONFIG_DMRID=5973272 CONFIG_CALLSIGN=TURBO CONFIG_FREQUENCY=433500000
 echo "Downloading modified HostFilesUpdate.sh..."
 curl --fail -o /usr/local/sbin/HostFilesUpdate.sh https://github.com/krot4u/Public_scripts/blob/master/HostFilesUpdate.sh
 /usr/local/sbin/HostFilesUpdate.sh
@@ -7,11 +7,15 @@ curl --fail -o /usr/local/sbin/HostFilesUpdate.sh https://github.com/krot4u/Publ
 curl --fail -o /etc/dmrgateway https://github.com/krot4u/Public_scripts/blob/master/dmrgateway.ini
 curl --fail -o /etc/mmdvmhost https://github.com/krot4u/Public_scripts/blob/master/mmdvmhost.ini
 
-sed -i "s/--Frequency--/${CONFIG_FREQUENCY}/" /etc/dmrgateway
-sed -i "s/--DMRID--/${CONFIG_DMRID}/" /etc/dmrgateway
+DMRID="${CONFIG_DMRID:-7777777}"
+FREQUENCY="${CONFIG_FREQUENCY:-431500000}"
+CALLSIGN="${CONFIG_CALLSIGN:-000}"
 
-sed -i "s/--CALLSIGN--/Type=${CONFIG_CALLSIGN}/" /etc/mmdvmhost
-sed -i "s/--DMRID--/Type=${CONFIG_DMRID}/" /etc/mmdvmhost
+sed -i "s/--Frequency--/$FREQUENCY/" /etc/dmrgateway
+sed -i "s/--DMRID--/$DMRID/" /etc/dmrgateway
+
+sed -i "s/--CALLSIGN--/Type=$CALLSIGN/" /etc/mmdvmhost
+sed -i "s/--DMRID--/Type=$DMRID/" /etc/mmdvmhost
 
 echo "Run pi-star update..."
 /usr/local/sbin/pistar-update
