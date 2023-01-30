c4de1c6427592e5fca02882a910b8114b20e1424
Victor Kro <krot4u@gmail.com>
2023-01-30 16:12:34 +0300

    HostFilesUpdate.sh created

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Test..."
+
+sed -i "s/curl --fail -o \$\{DMRHOSTS\}/#curl --fail -o \$\{DMRHOSTS\}/" /usr/local/sbin/HostFilesUpdate.sh
+
+sed -i "s/curl --fail -o /$\{DMRHOSTS}/#curl --fail -o /$\{DMRHOSTS}/" ./HostFilesUpdate.sh
