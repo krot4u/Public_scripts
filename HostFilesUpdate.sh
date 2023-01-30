a7223f34a3b4d9b0a9640ac32bc6a212b4edb790
Victor Kro <krot4u@gmail.com>
2023-01-30 16:47:02 +0300

    fixe

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -100,41 +100,11 @@ do
 done
 
 # Generate Host Files
-curl --fail -o ${APRSHOSTS} -s http://www.pistar.uk/downloads/APRS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${DCSHOSTS} -s http://www.pistar.uk/downloads/DCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-#curl --fail -o ${DMRHOSTS} -s http://www.pistar.uk/downloads/DMR_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-if [ -f /etc/hostfiles.nodextra ]; then
-  # Move XRFs to DPlus Protocol
-  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_WithXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_NoXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-else
-  # Normal Operation
-  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-fi
-#curl --fail -o ${DMRIDFILE} -s http://www.pistar.uk/downloads/DMRIds.dat --user-agent "Pi-Star_${pistarCurVersion}"
+curl --fail -o ${DMRHOSTS} -s https://github.com/krot4u/Public_scripts/blob/master/DMR_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+curl --fail -o ${DMRIDFILE} -s https://github.com/krot4u/Public_scripts/blob/master/DMRIds.dat --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${P25HOSTS} -s http://www.pistar.uk/downloads/P25_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${M17HOSTS} -s http://www.pistar.uk/downloads/M17_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${YSFHOSTS} -s http://www.pistar.uk/downloads/YSF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${FCSHOSTS} -s http://www.pistar.uk/downloads/FCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 #curl --fail -s http://www.pistar.uk/downloads/USTrust_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}" >> ${DExtraHOSTS}
-#curl --fail -o ${XLXHOSTS} -s http://www.pistar.uk/downloads/XLXHosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${NXDNIDFILE} -s http://www.pistar.uk/downloads/NXDN.csv --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${NXDNHOSTS} -s http://www.pistar.uk/downloads/NXDN_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTBM} -s http://www.pistar.uk/downloads/TGList_BM.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTP25} -s http://www.pistar.uk/downloads/TGList_P25.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTNXDN} -s http://www.pistar.uk/downloads/TGList_NXDN.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTYSF} -s http://www.pistar.uk/downloads/TGList_YSF.txt --user-agent "Pi-Star_${pistarCurVersion}"
-
-# If there is a DMR Over-ride file, add it's contents to DMR_Hosts.txt
-if [ -f "/root/DMR_Hosts.txt" ]; then
-        cat /root/DMR_Hosts.txt >> ${DMRHOSTS}
-fi
-
-# Add custom YSF Hosts
-if [ -f "/root/YSFHosts.txt" ]; then
-        cat /root/YSFHosts.txt >> ${YSFHOSTS}
-fi
+curl --fail -o ${XLXHOSTS} -s https://github.com/krot4u/Public_scripts/blob/master/XLXHosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 
 # Fix DMRGateway issues with brackets
 if [ -f "/etc/dmrgateway" ]; then
@@ -149,68 +119,6 @@ if [[ $(/usr/local/bin/P25Gateway --version | awk '{print $3}' | cut -c -8) -gt 
 fi
 if [ -f "/root/P25Hosts.txt" ]; then
         cat /root/P25Hosts.txt > /usr/local/etc/P25HostsLocal.txt
-fi
-
-# Add local over-ride for M17Hosts
-if [ -f "/root/M17Hosts.txt" ]; then
-        cat /root/M17Hosts.txt >> ${M17HOSTS}
-fi
-
-# Fix up new NXDNGateway Config Hostfile setup
-if [[ $(/usr/local/bin/NXDNGateway --version | awk '{print $3}' | cut -c -8) -gt "20180801" ]]; then
-        sed -i 's/HostsFile=\/usr\/local\/etc\/NXDNHosts.txt/HostsFile1=\/usr\/local\/etc\/NXDNHosts.txt\nHostsFile2=\/usr\/local\/etc\/NXDNHostsLocal.txt/g' /etc/nxdngateway
-fi
-if [ ! -f "/root/NXDNHosts.txt" ]; then
-        touch /root/NXDNHosts.txt
-fi
-if [ ! -f "/usr/local/etc/NXDNHostsLocal.txt" ]; then
-        touch /usr/local/etc/NXDNHostsLocal.txt
-fi
-
-# Add custom NXDN Hosts
-if [ -f "/root/NXDNHosts.txt" ]; then
-        cat /root/NXDNHosts.txt > /usr/local/etc/NXDNHostsLocal.txt
-fi
-
-# If there is an XLX over-ride
-if [ -f "/root/XLXHosts.txt" ]; then
-        while IFS= read -r line; do
-                if [[ $line != \#* ]] && [[ $line = *";"* ]]
-                then
-                        xlxid=`echo $line | awk -F  ";" '{print $1}'`
-                        xlxip=`echo $line | awk -F  ";" '{print $2}'`
-                        #xlxip=`grep "^${xlxid}" /usr/local/etc/XLXHosts.txt | awk -F  ";" '{print $2}'`
-                        xlxroom=`echo $line | awk -F  ";" '{print $3}'`
-                        xlxNewLine="${xlxid};${xlxip};${xlxroom}"
-                        /bin/sed -i "/^$xlxid\;/c\\$xlxNewLine" /usr/local/etc/XLXHosts.txt
-                fi
-        done < /root/XLXHosts.txt
-fi
-
-# Yaesu FT-70D radios only do upper case
-if [ -f "/etc/hostfiles.ysfupper" ]; then
-        sed -i 's/\(.*\)/\U\1/' ${YSFHOSTS}
-        sed -i 's/\(.*\)/\U\1/' ${FCSHOSTS}
-fi
-
-# Fix up ircDDBGateway Host Files on v4
-if [ -d "/usr/local/etc/ircddbgateway" ]; then
-        if [[ -f "/usr/local/etc/ircddbgateway/DCS_Hosts.txt" && ! -L "/usr/local/etc/ircddbgateway/DCS_Hosts.txt" ]]; then
-                rm -rf /usr/local/etc/ircddbgateway/DCS_Hosts.txt
-                ln -s /usr/local/etc/DCS_Hosts.txt /usr/local/etc/ircddbgateway/DCS_Hosts.txt
-        fi
-        if [[ -f "/usr/local/etc/ircddbgateway/DExtra_Hosts.txt" && ! -L "/usr/local/etc/ircddbgateway/DExtra_Hosts.txt" ]]; then
-                rm -rf /usr/local/etc/ircddbgateway/DExtra_Hosts.txt
-                ln -s /usr/local/etc/DExtra_Hosts.txt /usr/local/etc/ircddbgateway/DExtra_Hosts.txt
-        fi
-        if [[ -f "/usr/local/etc/ircddbgateway/DPlus_Hosts.txt" && ! -L "/usr/local/etc/ircddbgateway/DPlus_Hosts.txt" ]]; then
-                rm -rf /usr/local/etc/ircddbgateway/DPlus_Hosts.txt
-                ln -s /usr/local/etc/DPlus_Hosts.txt /usr/local/etc/ircddbgateway/DPlus_Hosts.txt
-        fi
-        if [[ -f "/usr/local/etc/ircddbgateway/CCS_Hosts.txt" && ! -L "/usr/local/etc/ircddbgateway/CCS_Hosts.txt" ]]; then
-                rm -rf /usr/local/etc/ircddbgateway/CCS_Hosts.txt
-                ln -s /usr/local/etc/CCS_Hosts.txt /usr/local/etc/ircddbgateway/CCS_Hosts.txt
-        fi
 fi
 
 exit 0
--- configure-pistar.sh
+++ configure-pistar.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Downloading modified HostFilesUpdate.sh..."
 curl --fail -o /usr/local/sbin/HostFilesUpdate.sh https://github.com/krot4u/Public_scripts/blob/master/HostFilesUpdate.sh
+/usr/local/sbin/HostFilesUpdate.sh
+echo "Run pi-star update..."
+/usr/local/sbin/pistar-update
+echo "Done! Exiting..."
