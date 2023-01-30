7d792ebcc910adc050e157fdc750991b01d958f4
Victor Kro <krot4u@gmail.com>
2023-01-30 16:21:21 +0300

    add files

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl --fail -o /usr/local/sbin/HostFilesUpdate.sh 
+echo "Downloading modified HostFilesUpdate.sh..."
+curl --fail -o /usr/local/sbin/HostFilesUpdate.sh https://github.com/krot4u/Public_scripts/blob/master/HostFilesUpdate.sh
