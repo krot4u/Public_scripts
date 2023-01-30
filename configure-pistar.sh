a8ae1c9330998f4cca6a60f7b7976d06e8f4032c
Victor Kro <krot4u@gmail.com>
2023-01-30 21:27:20 +0300

    fix

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -40,21 +40,23 @@ curl --fail -o /usr/local/sbin/HostFilesUpdate.sh -s https://raw.githubuserconte
 echo "Stopping Services..."
 service_handle stop
 echo "Done"
-
+echo "------------"
 /bin/bash /usr/local/sbin/HostFilesUpdate.sh
 
 echo "Downloading modified dmrgateway and mmdvmhost..."
 curl --fail -o /etc/dmrgateway -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/dmrgateway.ini
 curl --fail -o /etc/mmdvmhost -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/mmdvmhost.ini
-
+echo "Done"
+echo "------------"
 echo "Updating dmrgateway and mmdvmhost..."
 sed -i "s/--Frequency--/$FREQUENCY/" /etc/dmrgateway
 sed -i "s/--DMRID--/$DMRID/" /etc/dmrgateway
 
-sed -i "s/--CALLSIGN--/Type=$CALLSIGN/" /etc/mmdvmhost
+sed -i "s/--CALLSIGN--/$CALLSIGN/" /etc/mmdvmhost
 sed -i "s/--DMRID--/Type=$DMRID/" /etc/mmdvmhost
-
+echo "Done"
+echo "------------"
 echo "Run pi-star update..."
 /usr/local/sbin/pistar-update
-
+echo "------------"
 echo "Done! Exiting..."
