9042955ab9c78932044480deea90ed0e4a98e11c
Victor Kro <krot4u@gmail.com>
2023-01-31 14:16:28 +0300

    fixse

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -9,50 +9,60 @@ if [ "$(id -u)" != "0" ];then
         exit 1
 fi
 
-read -p "Введите Ваш DMRID (7 цифр): " DMRID
-if [[ ( $DMRID != ^[[:digit:]]+$ ) && ( $(`echo ${DMRID} |awk '{print length}'`) -ne 7 ) ]];then
-	echo " "
-	echo -e "   ${RED}Ошибка: Неправильный DMR ID!${NC}" 1>&2
-	echo " "
-	exit 1
-else
-	echo "----->"
-	echo -e "   ${GRN}Ваш DMRID ${DMRID} ${NC}"
-	echo "----->"
-	echo " "
-fi
+read_dmrid() {
+	read -p "Введите Ваш DMRID (7 цифр): " DMRID
+	len=`echo ${DMRID} |awk '{print length}'`
+	if [[ ( ${DMRID} != ^[[:digit:]]+$ ) && ( $len -ne 7 ) ]];then
+		echo "----->"
+		echo -e "   ${RED}Ошибка: Неправильный DMRID!${NC}" 1>&2
+		echo "----->"
+		echo " "
+		exit 1
+	else
+		echo "----->"
+		echo -e "   ${GRN}Ваш DMRID ${DMRID} ${NC}"
+		echo "----->"
+		echo " "
+	fi
+}
 
-echo "Введите частоту приёма\передачи. Задаётся без разделителя"
-read -p "(6 символов. Пример:433500): " FREQUENCY
-echo ""
-if [[ ( $FREQUENCY != ^[[:digit:]]+$ ) && ( $(`echo ${FREQUENCY} |awk '{print length}'`) -ne 6 ) ]];then
-	echo " "
-	echo -e "   ${RED}Ошибка: Неверная частота!${NC}" 1>&2
-	echo " "
-	exit 1
-else
-	echo "----->"
-	echo -e "   ${GRN}Частота приёма\передачи ${FREQUENCY} ${NC}"
-	echo "----->"
-	echo " "
-	FREQUENCY="${FREQUENCY}000"
-fi
+read_callsign() {
+	read -p "Введите Ваш позывной ( <= 7 символов): " CALLSIGN
+	len=`echo ${CALLSIGN} |awk '{print length}'`
+	if [[ ( ${CALLSIGN} != ^[[:alnum:]]+$ ) && ( $len -gt 7 ) ]];then
+		echo "----->"
+		echo -e "   ${RED}Ошибка: Неправильный Позывной!${NC}" 1>&2
+		echo "----->"
+		echo " "
+		exit 1
+	else
+		echo "----->"
+		echo -e "   ${GRN}Ваш Позывной ${CALLSIGN} ${NC}"
+		echo "----->"
+		echo " "
+	fi
+}
 
-read -p "Введите Ваш позывной ( <= 7 символов): " CALLSIGN
-echo ""
-if [[ ( $CALLSIGN != ^[[:alnum:]]+$ ) && ( $(`echo ${CALLSIGN} |awk '{print length}'`) -gt 7 ) ]];then
-	echo " "
-	echo -e "   ${RED}Ошибка: Неправильный позывной!${NC}" 1>&2
-	echo " "
-	exit 1
-else
-	echo "----->"
-	echo -e "   ${GRN}Ваш позывной ${CALLSIGN} ${NC}"
-	echo "----->"
-	echo " "
-fi
+read_frequency() {
+	echo "Введите частоту приёма\передачи. Задаётся без разделителя"
+	read -p "(6 символов. Пример:433500): " FREQUENCY
+	len=`echo ${FREQUENCY} |awk '{print length}'`
+	if [[ ( ${FREQUENCY} != ^[[:digit:]]+$ ) && ( $len -ne 6 ) ]];then
+		echo "----->"
+		echo -e "   ${RED}Ошибка: Неверная частота!${NC}" 1>&2
+		echo "----->"
+		echo " "
+		exit 1
+	else
+		echo "----->"
+		echo -e "   ${GRN}Частота приёма\передачи ${FREQUENCY} ${NC}"
+		echo "----->"
+		echo " "
+		FREQUENCY="${FREQUENCY}000"
+	fi
+}
 
-echo "${CALLSIGN} -- ${FREQUENCY} -- ${DMRID}"
+
 
 service_handle() {
 	# What do we want do to?
@@ -78,6 +88,10 @@ service_handle() {
 	systemctl ${doWhat} dstarrepeater.service 2> /dev/null 2>&1
 	systemctl ${doWhat} mmdvmhost.service && sleep 3 > /dev/null 2>&1
 }
+
+read_dmrid
+read_callsign
+read_frequency
 
 echo "Downloading modified HostFilesUpdate.sh..."
 curl -H 'Cache-Control: no-cache, no-store' --fail -o /usr/local/sbin/HostFilesUpdate.sh -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/HostFilesUpdate.sh
