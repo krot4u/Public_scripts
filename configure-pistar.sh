62e0384bd3e81275b7bed384228f767359cf58fc
Victor Kro <krot4u@gmail.com>
2023-01-31 16:36:07 +0300

    fixe

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -9,7 +9,7 @@ if [ "$(id -u)" != "0" ];then
         exit 1
 fi
 
-mount -o remount,rw / ; mount -o remount,rw /boot
+sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
 
 read_dmrid() {
 	while true; do
@@ -74,25 +74,25 @@ service_handle() {
 	# What do we want do to?
 	doWhat=${1}
 
-	systemctl ${doWhat} pistar-watchdog.service 2> /dev/null 2>&1
-	systemctl ${doWhat} pistar-remote.service 2> /dev/null 2>&1
-	systemctl ${doWhat} dmrgateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} dapnetgateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ircddbgateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} timeserver.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ysfgateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ysf2dmr.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ysf2nxdn.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ysf2p25.service 2> /dev/null 2>&1
-	systemctl ${doWhat} ysfparrot.service 2> /dev/null 2>&1
-	systemctl ${doWhat} dmr2ysf.service 2> /dev/null 2>&1
-	systemctl ${doWhat} dmr2nxdn.service 2> /dev/null 2>&1
-	systemctl ${doWhat} p25gateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} p25parrot.service 2> /dev/null 2>&1
-	systemctl ${doWhat} nxdngateway.service 2> /dev/null 2>&1
-	systemctl ${doWhat} nxdnparrot.service 2> /dev/null 2>&1
-	systemctl ${doWhat} dstarrepeater.service 2> /dev/null 2>&1
-	systemctl ${doWhat} mmdvmhost.service && sleep 3 > /dev/null 2>&1
+	sudo systemctl ${doWhat} pistar-watchdog.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} pistar-remote.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} dmrgateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} dapnetgateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ircddbgateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} timeserver.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ysfgateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ysf2dmr.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ysf2nxdn.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ysf2p25.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} ysfparrot.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} dmr2ysf.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} dmr2nxdn.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} p25gateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} p25parrot.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} nxdngateway.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} nxdnparrot.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} dstarrepeater.service 2> /dev/null 2>&1
+	sudo systemctl ${doWhat} mmdvmhost.service && sleep 3 > /dev/null 2>&1
 }
 
 read_dmrid </dev/tty
@@ -100,37 +100,37 @@ read_callsign </dev/tty
 read_frequency </dev/tty
 
 echo "Downloading modified HostFilesUpdate.sh..."
-curl -H 'Cache-Control: no-cache, no-store' --fail -o /usr/local/sbin/HostFilesUpdate.sh -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/HostFilesUpdate.sh
+sudo curl -H 'Cache-Control: no-cache, no-store' --fail -o /usr/local/sbin/HostFilesUpdate.sh -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/HostFilesUpdate.sh
 
 echo "Stopping Services..."
 service_handle stop
 echo "Done"
 echo "------------"
-/bin/bash /usr/local/sbin/HostFilesUpdate.sh > /dev/null 2>&1
+sudo /bin/bash /usr/local/sbin/HostFilesUpdate.sh > /dev/null 2>&1
 
 echo "Backup /etc/dmrgateway and /etc/mmdvmhost"
-cp /etc/dmrgateway /etc/dmrgateway.$(date +%Y%m%d)
-cp /etc/mmdvmhost /etc/mmdvmhost.$(date +%Y%m%d)
+sudo cp /etc/dmrgateway /etc/dmrgateway.$(date +%Y%m%d)
+sudo cp /etc/mmdvmhost /etc/mmdvmhost.$(date +%Y%m%d)
 echo "Removing /etc/dmrgateway and /etc/mmdvmhost"
-rm -f /etc/dmrgateway
-rm -f /etc/mmdvmhost
+sudo rm -f /etc/dmrgateway
+sudo rm -f /etc/mmdvmhost
 
 echo "Downloading modified dmrgateway and mmdvmhost..."
-curl -H 'Cache-Control: no-cache, no-store' --fail -o /etc/dmrgateway -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/dmrgateway.ini
-curl -H 'Cache-Control: no-cache, no-store' --fail -o /etc/mmdvmhost -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/mmdvmhost.ini
+sudo curl -H 'Cache-Control: no-cache, no-store' --fail -o /etc/dmrgateway -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/dmrgateway.ini
+sudo curl -H 'Cache-Control: no-cache, no-store' --fail -o /etc/mmdvmhost -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/mmdvmhost.ini
 echo "Done"
 echo "------------"
 echo "Updating dmrgateway and mmdvmhost..."
-sed -i "s/--Frequency--/$FREQUENCY/" /etc/dmrgateway
-sed -i "s/--DMRID--/$DMRID/" /etc/dmrgateway
+sudo sed -i "s/--Frequency--/$FREQUENCY/" /etc/dmrgateway
+sudo sed -i "s/--DMRID--/$DMRID/" /etc/dmrgateway
 
-sed -i "s/--CALLSIGN--/$CALLSIGN/" /etc/mmdvmhost
-sed -i "s/--DMRID--/$DMRID/" /etc/mmdvmhost
-sed -i "s/--Frequency--/$FREQUENCY/" /etc/mmdvmhost
+sudo sed -i "s/--CALLSIGN--/$CALLSIGN/" /etc/mmdvmhost
+sudo sed -i "s/--DMRID--/$DMRID/" /etc/mmdvmhost
+sudo sed -i "s/--Frequency--/$FREQUENCY/" /etc/mmdvmhost
 echo "Done"
 echo "------------"
 echo "Run pi-star update..."
-/usr/local/sbin/pistar-update
+sudo /usr/local/sbin/pistar-update
 echo "------------"
 echo "Done! Exiting..."
 
