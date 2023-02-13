55b4de81e3c42676b9ee034f2c65a2b9d1c545ed
Victor Kro <krot4u@gmail.com>
2023-02-13 15:30:35 +0300

    cleanup

--- DMRid/getdmr_id.ps1
+++ DMRid/getdmr_id.ps1
@@ -1,5 +1,3 @@
-#!/usr/bin/env pwsh
-
 #apt update  && apt install -y curl gnupg apt-transport-https
 # curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 # sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
