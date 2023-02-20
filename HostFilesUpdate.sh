f9669d7dc48b41f0260097a5ac89ffb2e47d8eb2
Victor Kro <krot4u@gmail.com>
2023-02-21 00:06:17 +0300

    fix

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -78,7 +78,7 @@ echo "------------"
 
 echo ">> HostFilesUpdate: Download dashboard files"
 # Pi-Star Dashboar modifications
-curl --fail -o '/var/www/dashboard/index.php' "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/index.php"
+curl --fail -o '/var/www/dashboard/index.php' -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/index.php"
 curl --fail -o '/var/www/dashboard/css/pistar-css.php' -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/css/pistar-css.php"
 curl --fail -o '/var/www/dashboard/lang/russian_ru.php' -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/lang/russian_ru.php"
 curl --fail -o '/var/www/dashboard/lang/english_uk.php' -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/lang/english_uk.php"
