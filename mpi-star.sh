7ffe33896b4221e66b6a78dd201e4d45ae376d75
Victor Kro <krot4u@gmail.com>
2023-02-22 18:29:10 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/index.php" > '/var/www/dashboard/index.php'
 curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/admin/index.php" > '/var/www/dashboard/admin/index.php'
+curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/dashboard/admin/expert/index.php" > '/var/www/dashboard/admin/expert/index.php'
 
 #IP=$(curl -s http://ipinfo.io/ip)
 #ORG=$(curl -s http://ipinfo.io/org)
