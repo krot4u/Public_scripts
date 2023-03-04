6814795704493ea168e85124705680755df5b18a
krot4u <krot4u@gmail.com>
2023-03-04 15:54:06 +0300

    Create configure-withBM.sh

---
+++ configure-withBM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dmrgateway=/etc/dmrgateway
+
+echo "Configuring Hosts files"
+echo "496;46.17.42.12;4001" >> ~/XLXHosts.txt
+echo "XLX_496	0000	46.17.42.12	passw0rd	62030" >> ~/DMR_Hosts.txt
+echo "------------"
+
+echo "Configuring INI files"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Startup=.*/Startup=496/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Enabled=.*/Enabled=1/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Port=.*/Port=62030/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Password=.*/Password=passw0rd/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Slot=.*/Slot=2/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^TG=.*/TG=9/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Base=.*/Base=94000/' "${dmrgateway}"
+sed -i -E '/^\[XLX Network\]$/,/^\[/ s/^Module=.*/Module=A/' "${dmrgateway}"
+echo "------------"
