5cb1eb60dac5312ef826467c72d5ea5601e2ffa6
Victor Kro <krot4u@gmail.com>
2023-03-28 10:43:16 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-curl --fail -o '/usr/local/sbin/HostFilesUpdate.sh' -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/HostFilesUpdate.sh
+
+CALLSIGN=$(sudo cat /etc/mmdvmhost | grep Callsign=TASKENT)
+
+if [[ "x$CALLSIGN" == "xTASKENT" ]] ; then
+  ## set directly
+  rpi-rw
+  sed -i "s/[Voice]\\nEnabled=0/[Voice]\\nEnabled=1/" /etc/dmrgateway
+  rpi-ro
+else
+  exit 0
+fi
