62c2363edf78c029eae6deaa47b9d641898cd9bd
Victor Kro <krot4u@gmail.com>
2023-03-28 10:44:32 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -5,7 +5,8 @@ CALLSIGN=$(sudo cat /etc/mmdvmhost | grep Callsign=TASKENT)
 if [[ "x$CALLSIGN" == "xTASKENT" ]] ; then
   ## set directly
   rpi-rw
-  sed -i "s/[Voice]\\nEnabled=0/[Voice]\\nEnabled=1/" /etc/dmrgateway
+
+  sed -i -E "/^\[Voice\]$/,/^\[/ s/^Enabled=.*/Enabled=1/" /etc/dmrgateway
   rpi-ro
 else
   exit 0
