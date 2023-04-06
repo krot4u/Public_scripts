5a5fa3523fca3a19b9af78a183f4da0d4014d459
Victor Kro <krot4u@gmail.com>
2023-04-06 17:15:23 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -10,11 +10,15 @@ else
 fi
 
 
-pistarHardware=$(awk -F "= " '/Hardware/ {print $2}' /etc/pistar-release)
-if [[ $pistarHardware == "OrangePiZero" ]]
+pistarCurVersion=$(awk -F "= " '/Version/ {print $2}' /etc/pistar-release)
+if [[ ${pistarCurVersion} == "4.1.4" ]]
+  then
+  pistarHardware=$(awk -F "= " '/Hardware/ {print $2}' /etc/pistar-release)
+  if [[ ${pistarHardware} == "OrangePiZero" ]]
   then
     curl -s -o /usr/local/sbin/pistar-upgrade https://raw.githubusercontent.com/AndyTaylorTweet/Pi-Star_Binaries_sbin/master/pistar-upgrade
     pistar-upgrade
   else
     exit 0  
+  fi
 fi
