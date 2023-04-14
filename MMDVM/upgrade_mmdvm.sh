f07e48ffc42f302ad90c8cfbfe0078c323cd0f37
Victor Kro <krot4u@gmail.com>
2023-04-14 18:59:46 +0300

    fix

--- MMDVM/upgrade_mmdvm.sh
+++ MMDVM/upgrade_mmdvm.sh
@@ -9,10 +9,15 @@ cp /usr/local/bin/MMDVMHost /usr/local/bin/MMDVMHost_origin
 cp /usr/local/bin/RemoteCommand /usr/local/bin/RemoteCommand_origin
 echo "------------"
 
+echo "Stop MMDVM service..."
+systemctl stop mmdvmhost.timer
+systemctl stop mmdvmhost.service
+echo "------------"
+
 echo "Downloading new MMDVMHost files..."
-curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/MMDVMCal" > '/usr/local/bin/MMDVMCal'
-curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/MMDVMHost" > '/usr/local/bin/MMDVMHost'
-curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/RemoteCommand" > '/usr/local/bin/RemoteCommand'
+curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/MMDVMCal" -o /usr/local/bin/MMDVMCal
+curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/MMDVMHost" -o /usr/local/bin/MMDVMHost
+curl --fail -s "https://raw.githubusercontent.com/krot4u/Public_scripts/master/MMDVM/RemoteCommand" -o /usr/local/bin/RemoteCommand
 echo "------------"
 echo " "
 echo "Done!"
