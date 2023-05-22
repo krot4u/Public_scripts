d3db27de334526e158bf8c8d8bca612fd609bba3
Victor Kro <krot4u@gmail.com>
2023-05-22 14:09:28 +0300

    fix qra+bm

--- configure-withBM.sh
+++ configure-withBM.sh
@@ -8,22 +8,22 @@ dmrgateway=/etc/dmrgateway
 echo "Set rpi-rw"
 mount -o remount,rw / ; mount -o remount,rw /boot
 
-if [[ "" == $(grep "dvs.qra-team.online" /usr/local/etc/P25HostsLocal.txt) ]]
+if [[ "" == $(grep "p25.qra-team.online" /usr/local/etc/P25HostsLocal.txt) ]]
   then
     echo "Configuring P25HostsLocal"
-    echo "14003	dvs.qra-team.online	41000" >> /usr/local/etc/P25HostsLocal.txt
+    echo "14003	p25.qra-team.online	41000" >> /usr/local/etc/P25HostsLocal.txt
 fi
 
-if [[ "" == $(grep "dvs.qra-team.online" /root/XLXHosts.txt) ]]
+if [[ "" == $(grep "qra-team.online" /root/XLXHosts.txt) ]]
   then
     echo "Configuring XLXHosts"
-    echo "496;dvs.qra-team.online;4001" >> /root/XLXHosts.txt
+    echo "496;qra-team.online;4001" >> /root/XLXHosts.txt
 fi
 
 if [[ "" == $(grep "dvs.qra-team.online" /root/DMR_Hosts.txt) ]]
   then
     echo "Configuring DMR_Hosts files"
-    echo "XLX_496       0000    dvs.qra-team.online     passw0rd        62030" >> /root/DMR_Hosts.txt
+    echo "XLX_496       0000    qra-team.online     passw0rd        62030" >> /root/DMR_Hosts.txt
 fi
 echo "------------"
 
@@ -44,22 +44,26 @@ if [ -z "$catxlx" ]
   sed -i '/# Add custom YSF Hosts/i if [ -f \"/root/XLXHosts.txt\" ]; then\n      cat /root/XLXHosts.txt >> ${XLXHOSTS}\nfi' /usr/local/sbin/HostFilesUpdate.sh
 fi
 
-dmridqra=`cat /usr/local/sbin/HostFilesUpdate.sh | grep 'krot4u/Public_scripts/master/DMRIds.dat'`
-if [ -z "$dmridqra" ]
-  then
-    sed -i '/DMRIds.dat --user-agent "Pi-Star_${pistarCurVersion}"/a curl --fail -o /usr/local/etc/DMRIdsQRA.dat -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/DMRIds.dat' /usr/local/sbin/HostFilesUpdate.sh
-    sed -i '/curl --fail -o \/usr\/local\/etc\/DMRIdsQRA.dat -s https:\/\/raw.githubusercontent.com\/krot4u\/Public_scripts\/master\/DMRIds.dat/a cat \/usr\/local\/etc\/DMRIdsQRA.dat >> \/usr\/local\/etc\/DMRIds.dat' /usr/local/sbin/HostFilesUpdate.sh
+dmridqra=$(grep 'krot4u/Public_scripts/master/DMRIds.dat' /usr/local/sbin/HostFilesUpdate.sh)
+
+if [ -z "$dmridqra" ]; then
+  sed -i "/DMRIds.dat --user-agent \"Pi-Star_\${pistarCurVersion}\"/a curl --fail -o /usr/local/etc/DMRIdsQRA.dat -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/DMRIds.dat" /usr/local/sbin/HostFilesUpdate.sh
+  sed -i "/curl --fail -o \/usr\/local\/etc\/DMRIdsQRA.dat -s https:\/\/raw.githubusercontent.com\/krot4u\/Public_scripts\/master\/DMRIds.dat/a cat \/usr\/local\/etc\/DMRIdsQRA.dat >> \/usr\/local\/etc\/DMRIds.dat" /usr/local/sbin/HostFilesUpdate.sh
 else
-  checkAlterPistar=$(cat /etc/svxlink/svxlink.conf | grep SimplexLogic)
-  if [ -z "$checkAlterPistar" ]
-    then
+  if [ -e "/etc/svxlink/svxlink.conf" ]; then
+    checkAlterPistar=$(grep SimplexLogic /etc/svxlink/svxlink.conf)
+    if [ -z "$checkAlterPistar" ]; then
       echo "Running pistar-update"
       pistar-update
+    else
+      echo "This is AlterPiStar"
+      /usr/local/sbin/HostFilesUpdate.sh
+      systemctl restart dmrgateway.service
+      systemctl restart mmdvmhost.service
+    fi
   else
-    echo "This is AlterPiStar"
-    /usr/local/sbin/HostFilesUpdate.sh
-    systemctl restart dmrgateway.service
-    systemctl restart mmdvmhost.service
+    echo "svxlink.conf file not found"
+    # Handle the absence of the file here, e.g., exit the script or perform alternative actions
   fi
 fi
 
