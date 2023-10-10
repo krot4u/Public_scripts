0d50d7dbd11e254b7a120fe2d7d87e3bfad91612
Viktor Krot <krot4u@gmail.com>
2023-10-10 23:31:00 +0300

    fix

--- configure-pistar.sh
+++ configure-pistar.sh
@@ -32,15 +32,21 @@ read_dmrid() {
 
 read_frequency() {
   while true; do
-    echo "Введите частоту приёма/передачи на ХотСпоте."
+    echo "Введите частоту приёма/передачи на ХотСпоте (разделитель точка)"
     read -p "(9 цифр. Пример: 433.500.000): " MFREQUENCY
     FREQUENCY=$(echo "${MFREQUENCY//./}")
+    FIRST=${MFREQUENCY:0:3}
     len=${#FREQUENCY}
     if ! [[ "$MFREQUENCY" =~ ^[0-9]{3}\.[0-9]{3}\.[0-9]{3}$ ]] || [ $len -ne 9 ]; then
       echo "----->"
       echo -e "   ${RED}Ошибка: Неверная частота!${NC}" 1>&2
       echo "----->"
       echo " "
+    else
+      echo " "
+    fi
+    if ! ([[ $FIRST -ge 144 && $FIRST -le 148 ]] || [[ $FIRST -ge 220 && $FIRST -le 225 ]] || [[ $FIRST -ge 420 && $FIRST -le 450 ]] || [[ $FIRST -ge 842 && $FIRST -le 950 ]]); then
+      echo -e "   ${RED}Ошибка: Эта частота не разрешена для использования!${NC}" 1>&2
     else
       echo "----->"
       echo -e "   ${GRN}Частота приёма/передачи ${MFREQUENCY} ${NC}"
