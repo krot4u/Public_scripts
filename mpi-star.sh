1c6be7373612e99f9608c858ae94520980c0fb82
Victor Kro <krot4u@gmail.com>
2023-10-13 23:53:16 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -4,24 +4,24 @@ mount -o remount,rw /
 mount -o remount,rw /boot
 
 ## -------- Fix DMR SelfOnly (Private HotSpot) --------- ##
-DMRID=$(awk -F'=' '/\[XLX Network\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/dmrgateway)
-if [[ ${DMRID} != 2500621 && ${DMRID} != 7700850 && ${DMRID} != 5973501 && ${DMRID} != 2120212 && ${DMRID} != 1000001]]; then
-  echo "Fix SelfOnly"
-  sed -i -E '/^\[DMR\]$/,/^\[/ s/^SelfOnly=0/SelfOnly=1/' "/etc/mmdvmhost"
-fi
+# DMRID=$(awk -F'=' '/\[XLX Network\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/dmrgateway)
+# if [[ ${DMRID} != 2500621 && ${DMRID} != 7700850 && ${DMRID} != 5973501 && ${DMRID} != 2120212 && ${DMRID} != 1000001]]; then
+#   echo "Fix SelfOnly"
+#   sed -i -E '/^\[DMR\]$/,/^\[/ s/^SelfOnly=0/SelfOnly=1/' "/etc/mmdvmhost"
+# fi
 
 ## -------- Fix Ping Server --------- ##
-curl --fail -s -o "/var/rrds/ping/ping.sh" -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/rrd/ping.sh
+#curl --fail -s -o "/var/rrds/ping/ping.sh" -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/rrd/ping.sh
 
-sudo crontab -l > /tmp/cronjob
-oldstring="\* \* \* \* \*  \/var\/rrds\/ping\/ping\.sh"
-newstring="\*\/10 \* \* \* \*  \/var\/rrds\/ping\/ping\.sh"
-sed -i -e "s@$oldstring@$newstring@" /tmp/cronjob
+# sudo crontab -l > /tmp/cronjob
+# oldstring="\* \* \* \* \*  \/var\/rrds\/ping\/ping\.sh"
+# newstring="\*\/10 \* \* \* \*  \/var\/rrds\/ping\/ping\.sh"
+# sed -i -e "s@$oldstring@$newstring@" /tmp/cronjob
 
-sudo crontab /tmp/cronjob
+# sudo crontab /tmp/cronjob
 
 ## -------- Add HBlink for Private Calls --------- ##
-sed -i '/^\[DMR Network 4\]/,/^$/d' /etc/dmrgateway
+#sed -i '/^\[DMR Network 4\]/,/^$/d' /etc/dmrgateway
 # sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' /etc/dmrgateway # remove empty line in the end
 # DMRID=$(awk -F'=' '/\[XLX Network\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/dmrgateway)
 #   cat <<EOF >> /etc/dmrgateway
@@ -41,5 +41,5 @@ sed -i '/^\[DMR Network 4\]/,/^$/d' /etc/dmrgateway
 # EOF
 
 ## --------- Fix Phantom TX --------- ##
-echo "Configuring INI files"
-sed -i -E '/^\[DMR Network\]$/,/^\[/ s/^Jitter=1000/Jitter=250/' "/etc/mmdvmhost"
+# echo "Configuring INI files"
+# sed -i -E '/^\[DMR Network\]$/,/^\[/ s/^Jitter=1000/Jitter=250/' "/etc/mmdvmhost"
