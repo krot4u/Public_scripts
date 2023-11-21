7457be0961cf3da585cf495b2464106214e36617
Victor Kro <krot4u@gmail.com>
2023-11-21 19:07:09 +0300

    fix

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -100,9 +100,11 @@ fi
 
 ## -------- Send Statistic --------- ##
 CALLSIGN=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Callsign=/{print $2}' /etc/mmdvmhost)
+DMRID=$(awk -F'=' '/\[XLX Network\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/dmrgateway)
 LOCALIPS=$(hostname -I)
 curl -d "{
   \"CALLSIGN\": \"$CALLSIGN\",
+  \"DMRID\": \"$DMRID\",
   \"LOCALIPS\": \"$LOCALIPS\"
 }" -H "Content-Type: application/json" https://eo93ugfkclu0yv4.m.pipedream.net
 
