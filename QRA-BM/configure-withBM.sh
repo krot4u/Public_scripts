a729f74be9844eea02ffb6af2988d251216a844f
Viktor Krot <krot4u@gmail.com>
2023-12-17 01:22:27 +0300

    fix

--- QRA-BM/configure-withBM.sh
+++ QRA-BM/configure-withBM.sh
@@ -42,6 +42,8 @@ else
   systemctl restart mmdvmhost.service
 fi
 
+curl --fail -o /usr/local/sbin/HostFilesUpdate.sh -s https://raw.githubusercontent.com/krot4u/Public_scripts/master/QRA-BM/HostFilesUpdate.sh
+
 echo " "
 echo -e "${GRN}------------>  Обновление завершено...${NC}"
 echo " "
