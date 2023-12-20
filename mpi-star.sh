ed298cff7c7870648bb338c7ad7a26377b1c1b15
Victor Kro <krot4u@gmail.com>
2023-12-20 18:35:40 +0300

    fix

--- mpi-star.sh
+++ mpi-star.sh
@@ -18,23 +18,9 @@ curl --fail -o /usr/local/sbin/pistar-upgrade -s https://raw.githubusercontent.c
 
 ## -------- Add HBlink for Private Calls --------- ##
 Exclude="
-2500621
-7700850
-5973501
-2120212
-1000001
 6660555
 1128888
-6660888
-6660444
-5973757
-5973842
-5973272
-7800555
-4852001
-4200042
 6556181
-2500782
 "
 DMRID=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/mmdvmhost)
 if echo ${EXCLUDE} | grep -q ${DMRID}; then
@@ -47,14 +33,17 @@ else
   sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' /etc/dmrgateway # remove empty line in the end
     cat <<EOF >> /etc/dmrgateway
 
-[DMR Network 4]
+[DMR Network 3]
 Enabled=1
-Name=QRA-hblink
+Name=QRA-Test
 Id=${DMRID}
 Address=hbl.qra-team.online
-Port=62030
-Password=QraDMRfree
+Port=62033
+Password=SomeTest38876
 TGRewrite0=2,597301,2,597301,1
+TGRewrite1=2,597302,2,597302,1
+TGRewrite2=2,597303,2,597303,1
+TGRewrite3=2,9999,2,9999,1
 PassAllPC0=1
 PassAllPC1=2
 Debug=0
@@ -63,51 +52,41 @@ EOF
 fi
 
 ## --------- Add Test Network --------- ##
-TESTING="
-7800555
-5973272
-4852001
-5973842
-5973757
-9200015
-4200042
-2500782
-"
-DMRID=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/mmdvmhost)
-if echo ${TESTING} | grep -q ${DMRID}; then
-  sed -i '/^\[DMR Network 3\]/,/^$/d' /etc/dmrgateway
-  sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' /etc/dmrgateway # remove empty line in the end
-    cat <<EOF >> /etc/dmrgateway
+# TESTING="
+# 7800555
+# 5973272
+# 4852001
+# 5973842
+# 5973757
+# 9200015
+# 4200042
+# 2500782
+# "
+# DMRID=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Id=/{print $2}' /etc/mmdvmhost)
+# if echo ${TESTING} | grep -q ${DMRID}; then
+#   sed -i '/^\[DMR Network 3\]/,/^$/d' /etc/dmrgateway
+#   sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' /etc/dmrgateway # remove empty line in the end
+#     cat <<EOF >> /etc/dmrgateway
 
-[DMR Network 3]
-Enabled=1
-Name=QRA-Test
-Id=${DMRID}
-Address=hbl.qra-team.online
-Port=62033
-Password=SomeTest38876
-TGRewrite0=2,433,2,433,1
-TGRewrite1=2,434,2,434,1
-TGRewrite2=2,9999,2,9999,1
-PassAllPC0=1
-PassAllPC1=2
-Debug=0
-Location=0
-
-[DMR Network 4]
-Enabled=1
-Name=QRA-hblink
-Id=${DMRID}
-Address=hbl.qra-team.online
-Port=62030
-Password=QraDMRfree
-TGRewrite0=2,597302,2,597302,1
-Debug=0
-Location=0
-EOF
-else
-  echo "Do nothing!"
-fi
+# [DMR Network 3]
+# Enabled=1
+# Name=QRA-Test
+# Id=${DMRID}
+# Address=hbl.qra-team.online
+# Port=62033
+# Password=SomeTest38876
+# TGRewrite0=2,597301,2,597301,1
+# TGRewrite1=2,597302,2,597302,1
+# TGRewrite2=2,597303,2,597303,1
+# TGRewrite3=2,9999,2,9999,1
+# PassAllPC0=1
+# PassAllPC1=2
+# Debug=0
+# Location=0
+# EOF
+# else
+#   echo "Do nothing!"
+# fi
 
 ## --------- Fix Phantom TX --------- ##
 # echo "Configuring INI files"
