713456eacb0fb87ebf06f35812ef79cf3b9acad8
Viktor Krot <krot4u@gmail.com>
2024-01-15 20:52:45 +0300

    fix

--- sendLogs.sh
+++ sendLogs.sh
@@ -3,10 +3,11 @@ export LC_ALL=C
 logs=$(ls /var/log/pi-star/*-$(date +"%Y-%m-%d").log)
 callsign=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Callsign=/{print $2}' /etc/mmdvmhost)
 tar=$callsign-logs-$(date +"%Y-%m-%d").tar.gz
-#token=$1
+token=$1
 
 tar -czvf $tar $logs 2> /dev/null
 
 curl \
-  -F "file=@$tar" \
-  "http://monitor.qra-team.online:8080"
+  -F "file=@./$tar" \
+  -F "token=$token" \
+  "http://monitor.qra-team.online:8080/upload"
