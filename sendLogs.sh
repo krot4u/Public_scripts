5492fdf17f53a46ddaf878ba02014177159a1bf5
Viktor Krot <krot4u@gmail.com>
2024-01-15 22:15:39 +0300

    fix

--- sendLogs.sh
+++ sendLogs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export LC_ALL=C
-logs=$(ls /var/log/pi-star/*-$(date +"%Y-%m-%d").log)
+
+data=$(date +"%Y-%m-%d")
+logs=$(ls /var/log/pi-star/*-$data.log)
 callsign=$(awk -F'=' '/\[General\]/{a=1; next} /\[/{a=0} a && /Callsign=/{print $2}' /etc/mmdvmhost)
-tar="/tmp/$callsign-logs-$(date +"%Y-%m-%d").tar.gz"
+tar="/tmp/$callsign-logs-$data.tar.gz"
 token=$1
 
 tar -czvf $tar $logs 2> /dev/null
